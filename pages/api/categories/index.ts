5a4d5d734ba266cdb96d66f5ced9aa72a7bce0ec
Johbrun <johan.brun@pm.me>
2020-04-21 22:22:15 +0200

    :bug: fix build

--- pages/api/categories/index.ts
+++ pages/api/categories/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import getCategories from '../../../lib/query/getCategories';
 import saveNewVisitor from '../../../lib/actions/saveNewVisitor';
 import { queryToVisitUser } from '../../../lib/helpers/queryToUserVisit';
+import { ErrorCodes } from '../errorCodes';
 
 module.exports = async (req: express.Request, res: express.Response) => 
 {
