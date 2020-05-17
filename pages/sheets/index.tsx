3c75f4d26092ed466e0f0c48cf11865bcccd7880
Johbrun <johan.brun@pm.me>
2020-05-17 21:40:19 +0200

    :sparkles: add pwa mode

--- pages/sheets/index.tsx
+++ pages/sheets/index.tsx
@@ -80,7 +80,7 @@ const SheetPage: NextPage<IProps> = ({ sheet, sheetsLight, categories }) =>
     return (
         <div className={classes.root}>
             <Head>
-                <title>PSEasy - Fiches PSE</title>
+                <title>PSEasy - Fiches PSE {sheet ? ' - ' + sheet.title : ''}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
