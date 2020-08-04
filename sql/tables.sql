0721ee63b99f4af6fc66ce364dd20b9dd2826ee1
Johbrun <johan.brun@pm.me>
2020-08-04 22:21:53 +0200

    :wrench: move sql dump + packagelock

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le :  Dim 24 mai 2020 à 16:17
--- Version du serveur :  5.7.29-0ubuntu0.18.04.1
--- Version de PHP :  7.2.24-0ubuntu0.18.04.3
+-- Hôte : localhost:3306
+-- Généré le : mar. 04 août 2020 à 19:57
+-- Version du serveur :  8.0.21-0ubuntu0.20.04.3
+-- Version de PHP : 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `pseasy`
+-- Base de données : `pseasy-dev`
 --
 
 -- --------------------------------------------------------
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `number` varchar(4) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -41,9 +41,9 @@ CREATE TABLE `category` (
 --
 
 CREATE TABLE `quizzAnswer` (
-  `id` int(5) NOT NULL,
-  `idQuestion` int(5) NOT NULL,
-  `idUser` varchar(255) DEFAULT NULL,
+  `id` int NOT NULL,
+  `idQuestion` int NOT NULL,
+  `idUser` varchar(255) NOT NULL,
   `answer1Choice` tinyint(1) DEFAULT NULL,
   `answer2Choice` tinyint(1) DEFAULT NULL,
   `answer3Choice` tinyint(1) DEFAULT NULL
@@ -56,9 +56,9 @@ CREATE TABLE `quizzAnswer` (
 --
 
 CREATE TABLE `quizzQuestion` (
-  `id` int(5) NOT NULL,
+  `id` int NOT NULL,
   `sheetReference` varchar(200) NOT NULL,
-  `difficulty` int(1) NOT NULL,
+  `difficulty` int NOT NULL,
   `question` varchar(512) NOT NULL,
   `explaination` varchar(2048) NOT NULL,
   `answer1` varchar(255) DEFAULT NULL,
@@ -76,10 +76,10 @@ CREATE TABLE `quizzQuestion` (
 --
 
 CREATE TABLE `quizzStat` (
-  `id` int(5) NOT NULL,
-  `idQuestion` int(5) NOT NULL,
-  `nbAnswers` int(4) NOT NULL DEFAULT '0',
-  `nbFirstOk` int(4) NOT NULL DEFAULT '0'
+  `id` int NOT NULL,
+  `idQuestion` int NOT NULL,
+  `nbAnswers` int NOT NULL DEFAULT '0',
+  `nbFirstOk` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -89,14 +89,14 @@ CREATE TABLE `quizzStat` (
 --
 
 CREATE TABLE `sheet` (
-  `id` int(4) NOT NULL,
+  `id` int NOT NULL,
   `reference` varchar(200) NOT NULL,
   `version` varchar(10) NOT NULL,
   `updatedDate` datetime NOT NULL,
   `title` varchar(400) NOT NULL,
-  `content` text CHARACTER SET utf32 NOT NULL,
-  `level` int(11) DEFAULT NULL,
-  `idCategory` int(10) NOT NULL,
+  `content` text CHARACTER SET utf32 COLLATE utf32_general_ci NOT NULL,
+  `level` int DEFAULT NULL,
+  `idCategory` int NOT NULL,
   `createdAdminDate` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -120,7 +120,7 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `visit` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `idUser` varchar(255) NOT NULL,
   `url` varchar(255) DEFAULT NULL,
   `date` datetime NOT NULL
@@ -186,31 +186,31 @@ ALTER TABLE `visit`
 -- AUTO_INCREMENT pour la table `quizzAnswer`
 --
 ALTER TABLE `quizzAnswer`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `quizzQuestion`
 --
 ALTER TABLE `quizzQuestion`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `quizzStat`
 --
 ALTER TABLE `quizzStat`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `sheet`
 --
 ALTER TABLE `sheet`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `visit`
 --
 ALTER TABLE `visit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
@@ -222,12 +222,6 @@ ALTER TABLE `visit`
 ALTER TABLE `quizzAnswer`
   ADD CONSTRAINT `quizzAnswer_ibfk_1` FOREIGN KEY (`idQuestion`) REFERENCES `quizzQuestion` (`id`),
   ADD CONSTRAINT `quizzAnswer_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`);
-
---
--- Contraintes pour la table `quizzStat`
---
-ALTER TABLE `quizzStat`
-  ADD CONSTRAINT `quizzStat_ibfk_1` FOREIGN KEY (`idQuestion`) REFERENCES `quizzQuestion` (`id`);
 
 --
 -- Contraintes pour la table `sheet`
