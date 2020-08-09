3806f74845eea953850668381d1a489ba8f7053f
Johbrun <johan.brun@pm.me>
2020-08-09 13:50:07 +0200

    :recycle: cut index into smal components

---
+++ components/homePicture.tsx
@@ -0,0 +1,103 @@
+
+import React from 'react';
+import {
+    makeStyles,
+    createStyles,
+    Typography,
+    Theme,
+    Container,
+    Grid,
+    Button,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        main: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'relative',
+            height: '500px',
+        },
+        background: {
+            position: 'absolute',
+            zIndex: -1,
+            height: '100%',
+            width: ' 100%',
+            objectFit: 'cover',
+        },
+        foreground: {
+            padding: theme.spacing(8, 0, 6),
+            color: 'white',
+        },
+        primaryTitle: {
+            fontSize: '5rem',
+            fontWeight: 400,
+            textShadow: '1px 1px 2px black',
+        },
+        secondaryTitle: {
+            fontWeight: 400,
+            color: 'white',
+            textShadow: '1px 1px 2px black',
+        },
+        mainButton: {
+            marginTop: theme.spacing(4),
+        },
+    }));
+
+interface IProps {
+    onClick: () => void
+}
+
+const HomePicture = (props: IProps) => 
+{
+    const classes = useStyles();
+    return (
+        <div className={classes.main}>
+            <img className={classes.background} src="cover.jpg" />
+            <div className={classes.foreground}>
+                <Container maxWidth="sm">
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        gutterBottom
+                        className={classes.primaryTitle}
+                    >PSEasy
+                    </Typography>
+                    <Typography
+                        variant="h5"
+                        align="center"
+                        color="textSecondary"
+                        className={classes.secondaryTitle}
+                        paragraph
+                    >
+                                    PSEasy est une application permettant la
+                                    consultation des recommandations et
+                                    référentiels relatifs au secourisme en
+                                    France.
+                    </Typography>
+                    <div className={classes.mainButton}>
+                        <Grid
+                            container
+                            spacing={2}
+                            justify="center"
+                        >
+                            <Grid item>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={props.onClick}
+                                >
+                                                Consulter les fiches
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
+};
+
+export default HomePicture;
--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -33,107 +33,70 @@ import { useRouter } from 'next/router';
 import Footer from '../../components/footer';
 import { postVisit } from '../../services/visit.service';
 import { IncomingMessage } from 'http';
+import HomePicture from '../../components/homePicture';
+import CardIcon from '../../components/cardIcon';
+import CardGrid, { ICard } from '../../components/cardGrid';
 
-const getIconElementFromName = (icon: IconsCard, classes: any) => {
-    switch (icon) {
-        case IconsCard.App:
-            return <PhoneIphoneIcon className={classes.cardMedia} />;
-        case IconsCard.Classement:
-            return <ClassIcon className={classes.cardMedia} />;
-        case IconsCard.Differences:
-            return <CompareArrowsIcon className={classes.cardMedia} />;
-        case IconsCard.Fiches:
-            return <BallotIcon className={classes.cardMedia} />;
-        case IconsCard.Historique:
-            return <ScheduleIcon className={classes.cardMedia} />;
-        case IconsCard.HorsLigne:
-            return <CloudOffIcon className={classes.cardMedia} />;
-        case IconsCard.Recherche:
-            return <SearchIcon className={classes.cardMedia} />;
-        case IconsCard.Comment:
-            return <CommentIcon className={classes.cardMedia} />;
-        case IconsCard.Quizz:
-            return <HelpOutlineIcon className={classes.cardMedia} />;
-    }
-};
-
-enum IconsCard {
-    Fiches,
-    Classement,
-    Recherche,
-    HorsLigne,
-    App,
-    Historique,
-    Differences,
-    Comment,
-    Quizz,
-}
-
-const cards: {
-    title: string;
-    content: string;
-    icon: IconsCard;
-    status: string;
-}[] = [
+const cards: ICard[] = [
     {
         title: 'Fiches PSE à jour',
         content:
             'Consultez la version des fiches du référentiel revu en Septembre 2019',
-        icon: IconsCard.Fiches,
+        icon: <CardIcon icon={<BallotIcon/>}/>,
         status: 'Opérationnel',
     },
     {
         title: 'Classement par catégories',
         content:
-            "Rerouvez facilement la fiche en utilisant l'affichage par catégorie",
-        icon: IconsCard.Classement,
+            'Retrouvez facilement la fiche en utilisant l\'affichage par catégorie',
+        icon: <CardIcon icon={<ClassIcon/>}/>,
         status: 'Opérationnel',
     },
     {
         title: 'Historique',
         content: 'Consultez les anciennes versions des fiches facilement',
-        icon: IconsCard.Historique,
+        icon: <CardIcon icon={<ScheduleIcon/>}/>,
         status: 'Opérationnel',
     },
     {
         title: 'Affichage des différences',
         content:
             'Affichez les nouveautés de contenu des fiches entre les différentes mises à jour',
-        icon: IconsCard.Differences,
+        icon: <CardIcon icon={<CompareArrowsIcon/>}/>,
         status: 'Opérationnel',
     },
     {
         title: 'Quizz',
         content: 'Testez vos connaissances du référentiel',
-        icon: IconsCard.Quizz,
+        icon: <CardIcon icon={<HelpOutlineIcon/>}/>,
+        status: 'Opérationnel',
+    },
+    {
+        title: 'Mode \'App\'',
+        content:
+            'Lancez l\'application directement depuis le menu de votre téléphone',
+        icon: <CardIcon icon={<PhoneIphoneIcon/>}/>,
         status: 'Opérationnel',
     },
     {
         title: 'Mode hors ligne',
-        content: "Profitez de l'application même sans connexion à l'Internet",
-        icon: IconsCard.HorsLigne,
-        status: 'Prévu T2 2020',
-    },
-    {
-        title: "Mode 'App'",
-        content:
-            "Lancez l'application directement depuis le menu de votre téléphone",
-        icon: IconsCard.App,
+        content: 'Profitez de l\'application même sans connexion à l\'Internet',
+        icon: <CardIcon icon={<CloudOffIcon/>}/>,
         status: 'Prévu T2 2020',
     },
     {
         title: 'Recherche par mot clé',
         content:
             'Retrouvez toutes les fiches en lien avec un ou plusieurs mots clés',
-        icon: IconsCard.Recherche,
-        status: 'Prévu T3 2020',
+        icon: <CardIcon icon={<SearchIcon/>}/>,
+        status: 'Prévu T4 2020',
     },
     {
         title: 'Echangez',
         content:
             'Une interrogation, un flou ? Surligner la partie intéressée, poser votre question, la communauté vous répondra !',
-        icon: IconsCard.Comment,
-        status: 'Prévu T3 2020',
+        icon: <CardIcon icon={<CommentIcon/>}/>,
+        status: 'Prévu T4 2020',
     },
 ];
 
@@ -152,87 +115,16 @@ const useStyles = makeStyles((theme: Theme) =>
         icon: {
             marginRight: theme.spacing(2),
         },
-        heroContent: {
-            padding: theme.spacing(8, 0, 6),
-            color: 'white',
-        },
-        heroButtons: {
-            marginTop: theme.spacing(4),
-        },
-        cardGrid: {
-            paddingTop: theme.spacing(8),
-            paddingBottom: theme.spacing(8),
-        },
-        card: {
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '10px',
-            textAlign: 'center',
-            '&:hover': {
-                color: ' rgb(62, 72, 110)',
-                boxShadow: 'rgba(121, 148, 212, 0.4) 0px 1rem 2rem',
-                transform: 'translateY(-2px)',
-            },
-        },
-        cardTitle: {
-            fontSize: '15px',
-            fontWeight: 800,
-            textTransform: 'uppercase',
-        },
-        cardMedia: {
-            paddingTop: '0.25%',
-            height: '115px !important',
-            fontSize: '82px !important',
-            color: theme.palette.primary.main,
-            margin: 'auto',
-        },
-        cardContent: {
-            flexGrow: 1,
-        },
-
-        divTitle: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            position: 'relative',
-            height: '500px',
-        },
-        imgTitle: {
-            position: 'absolute',
-            zIndex: -1,
-            height: '100%',
-            width: ' 100%',
-            objectFit: 'cover',
-        },
-        title1: {
-            fontSize: '5rem',
-            fontWeight: 400,
-            textShadow: '1px 1px 2px black',
-        },
-        title2: {
-            fontWeight: 400,
-            color: 'white',
-            textShadow: '1px 1px 2px black',
-        },
-        statusChip: {
-            color: 'white',
-            backgroundColor: theme.palette.primary.main,
-        },
-        statusChipNok: {
-            backgroundColor: 'red',
-        },
     })
 );
 
-const Home: NextPage<{}> = () => {
+const Home: NextPage<{}> = () => 
+{
     const classes = useStyles();
     const router = useRouter();
 
-    const handleClickSheets = () => {
-        router.push('/sheets');
-    };
-
+    const handleClickSheets = () => router.push('/sheets');
+    
     return (
         <div className={classes.root}>
             <Head>
@@ -247,127 +139,27 @@ const Home: NextPage<{}> = () => {
                     href="https://fonts.googleapis.com/icon?family=Material+Icons"
                 />
             </Head>
-            <React.Fragment>
-                <CssBaseline />
-                <SearchAppBar />
+            <CssBaseline />
+            <SearchAppBar />
 
-                <main>
-                    <div className={classes.drawerHeader} />
-                    {/* Hero unit */}
-                    <div className={classes.divTitle}>
-                        <img className={classes.imgTitle} src="cover.jpg" />
-                        <div className={classes.heroContent}>
-                            <Container maxWidth="sm">
-                                <Typography
-                                    component="h1"
-                                    variant="h2"
-                                    align="center"
-                                    gutterBottom
-                                    className={classes.title1}
-                                >
-                                    PSEasy
-                                </Typography>
-                                <Typography
-                                    variant="h5"
-                                    align="center"
-                                    color="textSecondary"
-                                    className={classes.title2}
-                                    paragraph
-                                >
-                                    PSEasy est une application permettant la
-                                    consultation des recommandations et
-                                    référentiels relatifs au secourisme en
-                                    France.
-                                </Typography>
-                                <div className={classes.heroButtons}>
-                                    <Grid
-                                        container
-                                        spacing={2}
-                                        justify="center"
-                                    >
-                                        <Grid item>
-                                            <Button
-                                                variant="contained"
-                                                color="primary"
-                                                onClick={handleClickSheets}
-                                            >
-                                                Consulter les fiches
-                                            </Button>
-                                        </Grid>
-                                    </Grid>
-                                </div>
-                            </Container>
-                        </div>
-                    </div>
-                    <Container className={classes.cardGrid} maxWidth="md">
-                        {/* End hero unit */}
-                        <Grid container spacing={4}>
-                            {cards.map((card) => (
-                                <Grid
-                                    item
-                                    key={card.title}
-                                    xs={12}
-                                    sm={6}
-                                    md={4}
-                                >
-                                    <Card className={classes.card}>
-                                        {/* {...card.icon} */}
-                                        {getIconElementFromName(
-                                            card.icon,
-                                            classes
-                                        )}
-                                        {/* <CameraIcon className={classes.cardMedia} />{" "} */}
-                                        {/* <Icon className={classes.cardMedia}>{card.icon}</Icon> */}
-                                        {/* <CardMedia
-                      className={classes.cardMedia}
-                      image="https://source.unsplash.com/random"
-                      title="Image title"
-                    /> */}
-                                        <CardContent
-                                            className={classes.cardContent}
-                                        >
-                                            <Typography
-                                                gutterBottom
-                                                className={classes.cardTitle}
-                                            >
-                                                {card.title}
-                                            </Typography>
-                                            <Typography>
-                                                {card.content}
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Chip
-                                                label={card.status}
-                                                className={clsx(
-                                                    classes.statusChip,
-                                                    {
-                                                        [classes.statusChipNok]:
-                                                            card.status !==
-                                                            'Opérationnel',
-                                                    }
-                                                )}
-                                            />
-                                        </CardActions>
-                                    </Card>
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </Container>
-                </main>
-
-                <Footer />
-            </React.Fragment>
+            <main>
+                <div className={classes.drawerHeader} />
+                <HomePicture onClick={handleClickSheets}/>
+                <CardGrid cards={cards}/>
+            </main>
+            
+            <Footer />
         </div>
     );
 };
 
-const getServerSideProps = async ( req : IncomingMessage ) => {
+const getServerSideProps = async ( req : IncomingMessage ) => 
+{
     console.log('GetInitialProps index');
     if (req) postVisit(req);
 
     return { props: {} };
 };
 
-export { getServerSideProps }
+export { getServerSideProps };
 export default Home;
